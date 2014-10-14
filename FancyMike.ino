119d654a0361d9c166cba1d8ae6005b286c15d4a
dennyabrain <denny.george90@gmail.com>
2014-10-14 12:33:59 -0400

    Done in ER without testing

--- FancyMike.ino
+++ FancyMike.ino
@@ -1,94 +1,65 @@
-/* 
- Debounce
- 
- Each time the input pin goes from LOW to HIGH (e.g. because of a push-button
- press), the output pin is toggled from LOW to HIGH or HIGH to LOW.  There's
- a minimum delay between toggles to debounce the circuit (i.e. to ignore
- noise).  
- 
- The circuit:
- * LED attached from pin 13 to ground
- * pushbutton attached from pin 2 to +5V
- * 10K resistor attached from pin 2 to ground
- 
- * Note: On most Arduino boards, there is already an LED on the board
- connected to pin 13, so you don't need any extra components for this example.
- 
- 
- created 21 November 2006
- by David A. Mellis
- modified 30 Aug 2011
- by Limor Fried
- modified 28 Dec 2012
- by Mike Walters
- 
- This example code is in the public domain.
- 
- http://www.arduino.cc/en/Tutorial/Debounce
- */
 
-// constants won't change. They're used here to 
-// set pin numbers:
-const int buttonPin = 2;    // the number of the pushbutton pin
-const int ledPin = 13;      // the number of the LED pin
+const int buttonPin = 2;    
+const int ledPin = 13;      
 
 // Variables will change:
-int ledState = HIGH;         // the current state of the output pin
-int buttonState;             // the current reading from the input pin
-int lastButtonState = LOW;   // the previous reading from the input pin
+int ledState = HIGH;         
+int buttonState;             
+int lastButtonState = LOW;   
 
 // the following variables are long's because the time, measured in miliseconds,
 // will quickly become a bigger number than can be stored in an int.
-long lastDebounceTime = 0;  // the last time the output pin was toggled
-long debounceDelay = 10;    // the debounce time; increase if the output flickers
+long lastDebounceTime = 0;  
+long debounceDelay = 10;    
+long lastOnTime=0;
 
 void setup() {
   pinMode(buttonPin, INPUT);
   pinMode(ledPin, OUTPUT);
   Serial.begin(9600);
-
   // set initial LED state
   digitalWrite(ledPin, ledState);
 }
 
 void loop() {
-  // read the state of the switch into a local variable:
   int reading = digitalRead(buttonPin);
 
-  // check to see if you just pressed the button 
-  // (i.e. the input went from LOW to HIGH),  and you've waited 
-  // long enough since the last press to ignore any noise:  
-
-  // If the switch changed, due to noise or pressing:
   if (reading != lastButtonState) {
-    // reset the debouncing timer
     lastDebounceTime = millis();
   } 
   
   if ((millis() - lastDebounceTime) > debounceDelay) {
-    // whatever the reading is at, it's been there for longer
-    // than the debounce delay, so take it as the actual current state:
-
-    // if the button state has changed:
     if (reading != buttonState) {
       buttonState = reading;
-
-      // only toggle the LED if the new button state is HIGH
       if (buttonState == LOW) {
         ledState = LOW ;
-        Serial.println("Magnet Near");
+        //Serial.println("Magnet Near");
+        long timeDiff=millis()-lastOnTime;
+        if(timeDiff>5){
+          Serial.write(5);
+        }
+        else if(timeDiff<5 && timeDiff>4){
+          Serial.write(4);
+        }
+        else if(timeDiff<4 && timeDiff>3){
+          Serial.write(3);
+        }
+        else if(timeDiff<3 && timeDiff>2){
+          Serial.write(2);
+        }
+        else if(timeDiff<2 && timeDiff>1){
+          Serial.write(1);
+        }
+        else if(timeDiff<1 && timeDiff>0){
+          Serial.write(0);
+        }        
       }
       else{
         ledState = HIGH;
       }
     }
   }
-  
-  // set the LED:
   digitalWrite(ledPin, ledState);
-
-  // save the reading.  Next time through the loop,
-  // it'll be the lastButtonState:
   lastButtonState = reading;
 }
 
--- SoundEffect/SoundEffect.pde
+++ SoundEffect/SoundEffect.pde
@@ -5,11 +5,18 @@ import ddf.minim.analysis.*;
 import ddf.minim.ugens.*;
 import ddf.minim.effects.*;
 
+import processing.serial.*;
+
 Minim minim;
 LiveInput in;
 AudioOutput out;
 Delay myDelay;
 BitCrush bitCrush;
+float delayTime;
+
+Serial myPort;
+int inByte;
+float param;
 
 void setup(){
   size(800,800);
@@ -21,22 +28,53 @@ void setup(){
   AudioStream inputStream = minim.getInputStream(out.getFormat().getChannels(),
                                                  out.bufferSize(),
                                                  out.sampleRate(),
-                                                 out.getFormat().getSampleSizeInBits());
-                                                 
+                                                 out.getFormat().getSampleSizeInBits());                                                 
  in = new LiveInput(inputStream);
-
- in.patch(myDelay).patch(bitCrush).patch(out);
+ //in.patch(myDelay).patch(bitCrush).patch(out);
+ in.patch(myDelay).patch(out);
+ println(Serial.list());
+ myPort = new Serial(this, "portname",9600);
 }
 
 void draw(){
+  background(#FFFF80);
+  text(delayTime,80,60);
   
 }
 
 void mouseMoved()
 {
   // set the delay time by the horizontal location
-  float delayTime = map( mouseY, 0, height, 0.0001, 0.5 );
-  myDelay.setDelTime( delayTime );
-  float bits = map( mouseX, 0, width, 1, 16 );
-  bitCrush.setBitRes(bits);
+  //delayTime = map( mouseY, 0, height, 0.0001, 1 );
+  //myDelay.setDelTime( delayTime );
+ // float bits = map( mouseX, 0, width, 1, 16 );
+ // bitCrush.setBitRes(bits);
+}
+
+void serialEvent(Serial myPort){
+  inByte= myPort.read();
+  println(inByte);
+  switch(inByte){
+    case 0:
+      param=0.5;
+      break;
+    case 1:
+      param=0.3;
+      break;
+    case 2:
+      param=0.2;
+      break;
+    case 3:
+      param=0.05;
+      break;
+    case 4:
+      param=0.005;
+      break;
+    case 5:
+      param=0.001;
+      break;
+    default:
+      break;
+  }
+  myDelay.setDelTime(param);
 }
